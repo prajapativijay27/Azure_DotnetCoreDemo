fa0e1795317fd38e4f1118e182c79549665b8aa2
prajapati vijay <prajapati.vijay271@outlook.com>
2024-07-17 11:42:18 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Azure_DotnetCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
